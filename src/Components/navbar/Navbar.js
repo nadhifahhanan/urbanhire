94f534f8021b80f84615389c62a57ae0bfef807f
nadhifahhanan <nadhifahhnn@gmail.com>
2019-05-27 23:11:17 +0700

    final result

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,64 +1,129 @@
 import React from 'react';
+import { Menu, Button, Image, Dropdown } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 import './Navbar.css'; 
 
+
 class Navbar extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="site-header">
-                <div className="container-fluid">
-                    <a href="https://www.urbanhire.com/" title="Urbanhire Home page" className="logo">
-                        <img src="https://winter-cdn.urbanhire.com/img/logo.svg" title="Urbanhire" alt="Urbanhire"/>
-                    </a>
-                    <a class="menu-toggle m-show">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </a>
-                    <nav class="site-nav">
-                        <div class="site-nav-header m-show-b">
-                            <span>Menu</span>
-                        </div>
-                        <ul class="menu">
-                            <li>
-                                <a href="https://www.urbanhire.com/" title="Urbanhire Home page">Home</a>
-                            </li>
-                            <li>
-                                <a href="https://www.urbanhire.com/features" title="Urbanhire ATS features">Features</a>
-                            </li>
-                            <li>
-                                <a href="https://www.urbanhire.com/applicant-tracking-system-pricing" title="Urbanhire ATS pricing" class="active">Pricing</a>
-                            </li>
-                            <li>
-                                <a href="https://www.urbanhire.com/customers" title="Urbanhire ATS customers">Customers</a>
-                            </li>
-                            <li>
-                                <a href="https://resources.urbanhire.com/" target="_blank">Resources</a>
-                            </li>
-                            <li class="has-children">
-                                <a href="/jobs">Job Search</a>
-                                <ul>
-                                    <li>
-                                        <a href="/signin" target="_blank">Log In as Jobseekers</a>
-                                    </li>
-                                    <li>
-                                        <a href="/signup" target="_blank">Register as Jobseekers</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="https://pro.urbanhire.com/signin">Log In</a>
-                            </li>
-                            <li>
-                                <a href="https://pro.urbanhire.com/signup" class="btn btn-6">Try for Free</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-      </div>
-    )
-  }
+    state = { }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <div>
+                <Menu ui secondary  menu size='large'>
+                    <Menu.Item className="icon-logo">
+                        <Image src='https://winter-cdn.urbanhire.com/img/logo.svg' href="https://www.urbanhire.com/" />
+                    </Menu.Item>
+                    <Menu.Menu position='right'>
+                        <Dropdown item icon='angle double down' simple>
+                            <Dropdown.Menu className='left'>
+                                <Dropdown.Item
+                                    name='home'
+                                    active={activeItem === 'home'}
+                                    onClick={this.handleItemClick}
+                                    href="https://www.urbanhire.com/">
+                                    Home
+                            </Dropdown.Item>
+                                <Dropdown.Item name='products'
+                                    active={activeItem === 'products'}
+                                    onClick={this.handleItemClick} 
+                                    href="https://www.urbanhire.com/features">
+                                    Products
+                            </Dropdown.Item>
+                                <Dropdown.Item
+                                    name='pricing'
+                                    active={activeItem === 'pricing'}
+                                    onClick={this.handleItemClick} 
+                                    href="https://www.urbanhire.com/applicant-tracking-system-pricing">
+                                    Pricing
+                            </Dropdown.Item>
+                                <Dropdown.Item
+                                    name='customers'
+                                    active={activeItem === 'customers'}
+                                    onClick={this.handleItemClick} 
+                                    href="https://www.urbanhire.com/customers">
+                                    Customers
+                            </Dropdown.Item>
+                                <Dropdown.Item
+                                    name='partners'
+                                    active={activeItem === 'partners'}
+                                    onClick={this.handleItemClick} 
+                                    href="https://www.urbanhire.com/partners">
+                                    Partners
+                            </Dropdown.Item>
+                                <Dropdown.Item
+                                    name='job search'
+                                    active={activeItem === 'job search'}
+                                    onClick={this.handleItemClick} 
+                                    href="https://www.urbanhire.com/jobs">
+                                    Job Search
+                            </Dropdown.Item>
+                                <Dropdown.Item
+                                    name='login'
+                                    active={activeItem === 'login'}
+                                    onClick={this.handleItemClick} 
+                                    href="https://pro.urbanhire.com/signin">
+                                    Log In
+                            </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <Button positive href="https://pro.urbanhire.com/signup?_ga=2.207819024.321113545.1558589636-562621735.1555067102">Try for Free</Button>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} href="https://www.urbanhire.com/"/>
+                        <Menu.Item
+                            name='products'
+                            active={activeItem === 'products'}
+                            onClick={this.handleItemClick}
+                            href="https://www.urbanhire.com/features"
+                        />
+                        <Menu.Item
+                            name='pricing'
+                            active={activeItem === 'pricing'}
+                            onClick={this.handleItemClick}
+                            href="https://www.urbanhire.com/applicant-tracking-system-pricing"
+                        />
+                        <Menu.Item
+                            name='customers'
+                            active={activeItem === 'customers'}
+                            onClick={this.handleItemClick}
+                            href="https://www.urbanhire.com/customers"
+                        />
+                        <Menu.Item
+                            name='partners'
+                            active={activeItem === 'partners'}
+                            onClick={this.handleItemClick}
+                            href="https://www.urbanhire.com/partners"
+                        />
+                        <Menu.Item
+                            name='job search'
+                            active={activeItem === 'job search'}
+                            onClick={this.handleItemClick}
+                            href="https://www.urbanhire.com/jobs"
+                        />
+                        <div className="navbarBorder"> </div>
+                        <Menu.Item
+                            name='login'
+                            active={activeItem === 'login'}
+                            onClick={this.handleItemClick}
+                            href="https://pro.urbanhire.com/signin"
+                        />
+                        <Menu.Item>
+                            <Button positive href="https://pro.urbanhire.com/signup?_ga=2.207819024.321113545.1558589636-562621735.1555067102"> Try for Free</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Menu>
+            </div>
+        )
+    }
 }
 
 export default Navbar;
+
+
+    
+    
--- src/Components/section/Section.js
+++ src/Components/section/Section.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { Image, Grid, Responsive} from 'semantic-ui-react'
-// import 'semantic-ui-css/semantic.js';
 import './Section.css';
 
 class Section extends React.Component {
     render() {
         return (
             <div className="layout-background">
-                <div className="description">
-                    <p className="title"> Assisting Companies Since 2016 </p>
-                        <Responsive as={Grid.Row} className="sub-description">
-                            <div className="margin-sub-description">
-                                <span className="totalCount"> 4,000 <span className="plus">+</span></span>
-                                <p className="sub-title"> Applicants Applied </p>
+                <div className="main">
+                    <p className="main-title"> Assisting Companies Since 2016 </p>
+                        <Responsive as={Grid.Row} className="main-content">
+                            <div className="margin-main-content">
+                                <span className="achievement-count"> 4,000 <span className="plus">+</span></span>
+                                <p className="achievement-desc"> Applicants Applied </p>
                             </div>
-                            <div className="margin-sub-description">
-                                <span className="totalCount"> 2,000 <span className="plus">+</span></span>
-                                <p className="sub-title"> Candidates Hired </p>
+                            <div className="margin-main-content">
+                                <span className="achievement-count"> 2,000 <span className="plus">+</span></span>
+                                <p className="achievement-desc"> Candidates Hired </p>
                             </div>
-                            <div className="margin-sub-description">
-                                <span className="totalCount"> 5,000 <span className="plus">+</span></span>
-                                <p className="sub-title"> Company Subscriptions </p>
+                            <div className="margin-main-content">
+                                <span className="achievement-count"> 5,000 <span className="plus">+</span></span>
+                                <p className="achievement-desc"> Company Subscriptions </p>
                             </div>
                         </Responsive>
                 </div>
-                <Image className="picture" src='https://winter-cdn.urbanhire.com/img/img-backgroundcity.svg' />
-                <Image className="picture-2" src='https://cdn1.imggmi.com/uploads/2019/5/26/765378c73611811c2e256f68bd75f5ad-full.png' />
+                <Image className="building-base" src='https://cdn1.imggmi.com/uploads/2019/5/27/5f23a448de7f1acf2cd4b3a51c8dd898-full.png' />
+                <Image className="deco-1" src='https://cdn1.imggmi.com/uploads/2019/5/27/45f10270539119d0af3b1c0f147ccf6b-full.png' />
+                <Image className='deco-2' src='https://cdn1.imggmi.com/uploads/2019/5/27/72f33c2841e3c43ebcaf804c970c2364-full.png' />
             </div >
         )
     }
